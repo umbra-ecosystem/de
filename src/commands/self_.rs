997df568e3861bec4cabc0235298588fe7682482
Haisham <--clear>
2025-06-25 21:06:10 +0500

    Remove smol and switch updater to blocking mode

--- src/commands/self_.rs
+++ src/commands/self_.rs
@@ -4,25 +4,22 @@ use eyre::{WrapErr, eyre};
 use crate::constants::PROJECT_NAME;
 
 pub fn update() -> eyre::Result<()> {
-    smol::block_on(async {
-        let mut updater = AxoUpdater::new_for(PROJECT_NAME);
-        updater
-            .load_receipt()
-            .map_err(|e| eyre!(e))
-            .wrap_err("Failed to initialize updater.")?;
+    let mut updater = AxoUpdater::new_for(PROJECT_NAME);
+    updater
+        .load_receipt()
+        .map_err(|e| eyre!(e))
+        .wrap_err("Failed to initialize updater.")?;
 
-        let updated = updater
-            .run()
-            .await
-            .map_err(|e| eyre!(e))
-            .wrap_err("Failed to run updater")?;
+    let updated = updater
+        .run_sync()
+        .map_err(|e| eyre!(e))
+        .wrap_err("Failed to run updater")?;
 
-        if let Some(updated) = updated {
-            println!("Updated to version: {}", updated.new_version_tag);
-        } else {
-            println!("No updates available.");
-        }
+    if let Some(updated) = updated {
+        println!("Updated to version: {}", updated.new_version_tag);
+    } else {
+        println!("No updates available.");
+    }
 
-        Ok(())
-    })
+    Ok(())
 }
